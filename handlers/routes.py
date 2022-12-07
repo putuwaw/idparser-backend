73f96bc845369d2f929a9ac6b1cc0858d8fa1eb1
Putu Widyantara <putuwaw973@gmail.com>
2022-12-08 01:16:26 +0800

    Create endpoint for testing

--- handlers/routes.py
+++ handlers/routes.py
@@ -1,9 +1,21 @@
-from flask import render_template
+from flask import request, jsonify
+from flask_cors import cross_origin
 from modules import modules
 
 def configure_routes(app):
     @app.route("/")
+    @cross_origin()
     def index():
         hello = modules.hello()
         content = modules.content()
-        return render_template("index.html", hello=hello, content=content)
+        return jsonify({"hello": hello, "content": content})
+
+    @app.route("/test", methods=['POST'])
+    @cross_origin()
+    def test():
+        requestString = request.get_json()
+        temp = requestString['string']
+        if (modules.is_palindrome(temp)):
+            return jsonify({'result': 'String palindrome'})
+        else:
+            return jsonify({'result': 'String tidak palindrome'})
