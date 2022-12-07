cf320a64167cea2a3d005beabffa23605c2f3bf6
Putu Widyantara <putuwaw973@gmail.com>
2022-12-08 01:16:39 +0800

    Create unit testing for test endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from handlers.routes import configure_routes
 
-
 def test_index():
-    app = Flask(__name__, template_folder="../templates")
+    app = Flask(__name__)
     configure_routes(app)
     app.testing = True
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
+
+def test_api():
+    app = Flask(__name__)
+    configure_routes(app)
+    app.testing = True
+    client = app.test_client()
+    response = client.post('/test', json={'string': 'katak'})
+    assert response.status_code == 200
+    assert response.get_json() == {'result': 'String palindrome'}
+    response = client.post('/test', json={'string': 'kata'})
+    assert response.status_code == 200
+    assert response.get_json() == {'result': 'String tidak palindrome'}
+
