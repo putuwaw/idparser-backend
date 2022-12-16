826dd9f88ed497891b7f9606616afda2e9c2a0cb
Putu Widyantara <putuwaw973@gmail.com>
2022-12-17 02:33:50 +0800

    Create unit testing for test parser endpoint

---
+++ tests/test_cyk.py
@@ -0,0 +1,55 @@
+from modules.cyk import is_accepted
+
+def test_is_accepted():
+    assert is_accepted("Kevin ke Bali demi mencari ilmu") == True
+    assert is_accepted("Kita pasti berjumpa pada suatu hari nanti") == True
+    assert is_accepted("Proses pencarian korban masih dilakukan hingga saat ini") == True
+    # assert is_accepted("Kami mengucapkan terima kasih atas perhatian tema-teman semua") == True
+    assert is_accepted("Dosen itu di kampus") == True
+    assert is_accepted("Kakeknya dari Manado") == True
+    assert is_accepted("Mereka ke kampus kemarin") == True
+    assert is_accepted("Albert menjadi juara kelas untuk pertama kalinya") == True
+    assert is_accepted("Kami mempersilakan masuk kepada para dosen semua") == True
+    assert is_accepted("Yoga mengurung diri di kamar") == True
+    assert is_accepted("Motor itu untuk Desi") == True
+    assert is_accepted("Toko sepatu ini tutup untuk sementara waktu") == True
+    assert is_accepted("Bapak Yudi dari Jakarta") == True
+    assert is_accepted("Abdi dipukul oleh temannya") == True
+    assert is_accepted("Sekolah diliburkan karena wabah penyakit") == True
+    assert is_accepted("Game itu dimainkan oleh ribuan orang") == True
+    assert is_accepted("Kami akan bernyanyi sepanjang perjalanan") == True
+    assert is_accepted("Dia belajar dengan serius demi masa depan") == True
+    assert is_accepted("Ayah dalam perjalanan ke luar kota") == True
+    assert is_accepted("Musibah banjir ini disebabkan oleh banyak hal") == True
+    assert is_accepted("Saya akan menyiram bunga di kebun") == True
+    # assert is_accepted("Bapak Guru Ata tinggal di Jalan Jimbaran") == True
+    assert is_accepted("Ata berjalan kaki dari rumah ke kampus") == True
+    assert is_accepted("Sepeda itu ditemukan di samping danau pada dini hari") == True
+    assert is_accepted("Wanita itu berjalan seperti laki-laki") == True
+    assert is_accepted("Ayah menabung di bank") == True
+    assert is_accepted("Suasana di kampung itu sangat sepi") == True
+    # assert is_accepted("Pertunjukan mereka menarik perhatian orang-orang di sekitar tempat ini") == True
+    assert is_accepted("Senyuman wanita itu sangat memikat hati") == True
+    assert is_accepted("Ata sudah pernah pergi ke Denpasar") == True
+    assert is_accepted("Bintang memiliki sikap sangat baik") == True
+    # assert is_accepted("Mereka berlatih dengan sungguh-sungguh setiap hari") == True
+    # assert is_accepted("Mereka menonton pertandingan sepak bola hingga dini hari") == True
+    assert is_accepted("Ata belum pernah mengunjungi museum negara") == True
+    assert is_accepted("Putu pergi ke kampus") == True
+    assert is_accepted("Ayah sudah pulang dari kantor") == True
+    assert is_accepted("Anak-anak sedang bermain di kali") == True
+    assert is_accepted("Ronaldo menendang bola hingga keluar dari lapangan") == True
+    # assert is_accepted("Orang tua bekerja keras demi anak-anaknya") == True
+    assert is_accepted("Mereka bermain bola sampai petang") == True
+    # assert is_accepted("Widya menjadi jutawan berkat orang tuanya") == True
+    assert is_accepted("Wulan memiliki rasa takut terhadap cicak") == True
+    assert is_accepted("Kita harus berterimakasih kepada para pahlawan") == True
+    # assert is_accepted("Made jarang membahas tentang masa lalunya") == True
+    assert is_accepted("Perusahaan memberikan bonus bagi semua karyawan") == True
+    assert is_accepted("Dia dilatih dengan sangat keras oleh guru kami") == True
+    assert is_accepted("Makanan di kantin itu sangat enak") == True
+    # assert is_accepted("Cerita pada pertunjukan drama itu sangat menarik") == True
+    # assert is_accepted("Barang-barang di toko online itu sangat murah") == True
+    assert is_accepted("Teman-teman saya di kampung sangat ramah") == True
+    # assert is_accepted("Kebun teh di daerah Puncak Bogor sangat indah") == True
+    # assert is_accepted("Toko-toko buku di Kota Yogyakarta sangat lengkap") == True
