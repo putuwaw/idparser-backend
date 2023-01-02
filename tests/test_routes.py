461fe2027d94eca20b73149570c65cd4976bf08d
Putu Widyantara <putuwaw973@gmail.com>
2023-01-02 18:30:22 +0800

    Remove unit testing for test endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,6 +1,4 @@
-from flask import Flask, jsonify, request
-from flask_cors import cross_origin
-from modules import modules
+from flask import Flask
 from handlers.routes import configure_routes
 
 def test_index():
@@ -10,33 +8,3 @@ def test_index():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-
-def test_api():
-    app = Flask(__name__)
-    with app.app_context():
-        @app.route("/test", methods=['POST'])
-        @cross_origin()
-        def test():
-            requestString = request.get_json()
-            temp = requestString['string']
-            graph = modules.draw_graph()
-            if (modules.is_palindrome(temp)):
-                return jsonify({
-                    'result': 'String palindrome', 
-                    'graph': graph.source,
-                })
-            else:
-                return jsonify({
-                    'result': 'String tidak palindrome'
-                })
-        app.testing = True
-        client = app.test_client()
-        response = client.post('/test', json={'string': 'katak'})
-        assert response.status_code == 200
-        assert response.get_json() == {
-            "graph": "digraph G {\n\tHello -> World\n\tHello -> Halo\n\tWorld -> Dunia\n}\n",
-            "result": "String palindrome"
-        }
-        response = client.post('/test', json={'string': 'kata'})
-        assert response.status_code == 200
-        assert response.get_json() == {'result': 'String tidak palindrome'}
